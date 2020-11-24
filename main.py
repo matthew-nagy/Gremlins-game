2e774406e5b150d3cb55eb32d488b35e5849bb77
Matthew Nagy <matttnagy@gmail.com>
2020-11-24 20:15:21 +0000

    Added a player to the game

--- main.py
+++ main.py
@@ -4,26 +4,38 @@ py.init()
 import ServerClient as net
 import level as l
 import sprite as s
+import player
 
-window = py.display.set_mode((600,600))
-white_screen_col = py.Color(255, 255, 255)
-fps_controller = py.time.Clock()
+def runGame():
+    window = py.display.set_mode((600,600))
+    white_screen_col = py.Color(255, 255, 255)
+    fps_controller = py.time.Clock()
 
-l = l.Level('map1.png', 20, (50, 50))
+    lvl = l.Level('map1.png', 20, (50, 50))
 
-spr = s.Sprite((200,200),s.drawRect,(window,5,5), py.Color(255,0,0))
+    p = player.Base_Player(300, 300, py.Color(255, 0, 150), window)
 
-play = True
-while play:
-    py.draw.rect(window, white_screen_col, (0,0,600,600))
-    l.draw(window)
-    spr.move(1,1)
-    spr.draw()
+    play = True
+    while play:
+        py.draw.rect(window, white_screen_col, (0,0,600,600))
+        lvl.draw(window)
+        p.draw()
 
-    py.display.update()
-    for event in py.event.get():
-        if event.type == QUIT:
-            py.quit()
-            play = False
-    
-    fps_controller.tick(30)
+        py.display.update()
+        for event in py.event.get():
+            if event.type == QUIT:
+                py.quit()
+                play = False
+                return
+        
+        player_input = player.Player_Input.generate()
+
+        if py.key.get_pressed()[py.K_9]:
+            p.applyMovement(p.getLastInput(), lvl)
+        else:
+            p.applyMovement(player_input, lvl)
+        
+        fps_controller.tick(30)
+
+
+runGame()
