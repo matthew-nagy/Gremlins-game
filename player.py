1e040aeea9bfb9a64ad51429bd7dc2345ef45d8f
Matthew Nagy <matttnagy@gmail.com>
2020-11-24 20:15:40 +0000

    Allowed the player to move and update of past input

--- player.py
+++ player.py
@@ -1,9 +1,38 @@
 import ServerClient as net
 import pygame as py
+import level as l
+import copy
+import sprite
 
 class state:
     dead = 0
     alive = 1
+
+#Used to store what a players input was on a particular frame
+class Player_Input:
+    def __init__(self, up, down, left, right, rotate_clockwise, rotate_anticlockwise, trigger):
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
+        self.rotate_clockwise = rotate_clockwise
+        self.rotate_anticlockwise = rotate_anticlockwise
+        self.trigger = trigger
+
+    @staticmethod
+    def generate():
+        inputs = [False, False, False, False, False, False, False] 
+        key_maps = [py.K_w, py.K_s, py.K_a, py.K_d, py.K_l, py.K_k, py.K_RETURN]
+        all_key_pressed_info = py.key.get_pressed()
+
+        count = 0
+        for key in key_maps:
+            if all_key_pressed_info[key]:
+                inputs[count] = True
+            count += 1
+
+        return Player_Input(inputs[0], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5], inputs[6])
+
 '''
 This is a snapshot of the state of a player in time
 They will be sent to and from the server and hosts, with the idea that when a client
@@ -34,21 +63,60 @@ class Positional_Data:
             return False
         return True
 
-#Used to store what a players input was on a particular frame
-class Player_Input:
-    def __init__(self, up, down, left, right, trigger):
-        self.up = up
-        self.down = down
-        self.left = left
-        self.right = right
-        self.trigger = trigger
+    def applyMovement(self, input, level):
+        x_movement = 0
+        y_movement = 0
+        if input.up:
+            y_movement = -1
+        elif input.down:
+            y_movement = 1
+        
+        if input.left:
+            x_movement = -1
+        elif input.right:
+            x_movement = 1
+        
+        if not level.is_collision(self.x + x_movement, self.y):
+            self.x += x_movement
+        if not level.is_collision(self.x, self.y + y_movement):
+            self.y += y_movement
+        rotation_speed = 2
+        if input.rotate_clockwise:
+            self.rotation += rotation_speed
+            if self.rotation > 360:
+                self.rotation -= 360
+        elif input.rotate_anticlockwise:
+            self.rotation -= rotation_speed
+            if self.rotation < 0:
+                self.rotation += 360
+        
+        #Finally incriment what frame this is
+        self.frameOn += 1
 
-class Player:
-    def __init__(self, x, y):
-        self.predictions = []
-        self.inputs = []
-        self.currentPosition = Positional_Data(x, y, 0, state.alive)
+class Base_Player:
+    def __init__(self, x, y, colour, window):
+        self.previous_positions = []#The last few positions it has been in
+        self.previous_inputs = []#The last few inputs the player recieved
+
+        self.current_input = Player_Input(0,0,0,0,0,0,0) #The current Input it thinks it has
+        self.current_position = Positional_Data(x, y, 0, state.alive)    #Current position
+        self.sprite = sprite.Sprite((x,y), sprite.drawCircle, (window, 3), colour)
     
     #sends by UDP because we don't want the entire system to freeze up
     def sendPositionData(self, connection):
-        connection.sendto(bytes(self.currentPosition))
+        connection.sendto(bytes(self.current_position))
+
+    def draw(self):
+        self.sprite.setPosition(self.current_position.x, self.current_position.y)
+        self.sprite.draw()
+    
+    #If they didn't 
+    def getLastInput(self):
+        return self.current_input
+    
+    def applyMovement(self, input, level):
+        self.previous_positions.append(copy.copy(self.current_position))
+        self.previous_inputs.append(copy.copy(self.current_input))
+
+        self.current_input = input
+        self.current_position.applyMovement(input, level)
